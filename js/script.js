f03b1f1aa0d68fc31c2fe64cbb2dfc5f31fb0f6d
Alex Oh <example@gmail.com>
2023-02-28 21:01:40 +0300

    exercis one - Donee

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-// For start project
-// npm init
-// npm init @eslint/config
-// 3 - 1 - 3 - no - browser - 2 - 1 - 1 - 2 - windows - yes - yes - npm - DONE
+'use strict';
 
-
-
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres:[],
+	privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('на сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('на сколько оцените его?', '');
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d; 
+console.log(personalMovieDB.movies);
