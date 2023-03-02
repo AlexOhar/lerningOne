a81b9af47f1b0bcfafb158bf09a6de8123f75cff
Alex Oh <example@gmail.com>
2023-03-02 23:54:00 +0300

    Done cycle with questions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+// again:
+// for (let i = 0; i < 2; i++) {
+// 	console.log(i);
+// 	let nameFilm = prompt('Один из последних просмотренных фильмов?', '');
+// 	if (nameFilm === '' || nameFilm == null || nameFilm.length > 10) {
+// 		i = -1;
+// 		continue again;
+// 	}
+// 	console.log(i);
+// 	let evaluation = prompt('на сколько оцените его?', '');
+// 	if (evaluation === '' || evaluation == null) {
+// 		i = -1;
+// 		continue again;
+// 	}
+// 	personalMovieDB.movies[nameFilm] = evaluation; 
+// }
+
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -8,12 +25,27 @@ const personalMovieDB = {
 	genres:[],
 	privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('на сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('на сколько оцените его?', '');
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d; 
-console.log(personalMovieDB.movies);
 
+for (let i = 0; i < 2; i++) {
+	let nameFilm = prompt('Один из последних просмотренных фильмов?', ''),
+		evaluation = prompt('на сколько оцените его?', '');
+	if (nameFilm != '' && evaluation != '' && evaluation != null && nameFilm != null && nameFilm.length < 50) {
+		personalMovieDB.movies[nameFilm] = evaluation; 
+		console.log('Done');
+	} else {
+		console.log('Error');
+		i--;
+	}
+}
+if (personalMovieDB.count <= 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB.movies);
 
